67e066d5aa1cf1e9511a4331e842e6ed1875a22a
abelHodasz <hodabi@gmail.com>
2020-03-02 15:27:31 +0100

    quicksort

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const HEIGHT = window.innerHeight / 2;
 const MIN = 25;
+const BAR_CONTAINER = document.querySelector("#bar-container");
+const SLIDER = document.querySelector("#slider");
+const QUICK_SORT_BUTTON = document.querySelector("#quick-sort");
+
 const mapRange = function(n, start1, stop1, start2, stop2) {
     return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
 };
@@ -47,36 +51,77 @@ const removeFromEnd = (intArray, times) => {
     }
 };
 
-const main = () => {
-    const barContainer = document.querySelector("#bar-container");
+const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
 
+//SORTING ALGORITHMS
+
+const switchItems = (array, i, j) => {
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+};
+
+//quicksort
+async function partition(array, low, high) {
+    let index = low - 1;
+    const pivot = array[high];
+
+    for (let i = low; i < high; i++) {
+        if (array[i] <= pivot) {
+            index++;
+            await sleep(200 - SLIDER.value);
+            switchItems(array, index, i);
+            updateBars(array);
+        }
+    }
+
+    switchItems(array, index + 1, high);
+    return index + 1;
+}
+
+async function quickSort(array, low, high) {
+    if (low < high) {
+        partitionIndex = await partition(array, low, high);
+
+        quickSort(array, low, partitionIndex - 1);
+        quickSort(array, partitionIndex + 1, high);
+    }
+}
+
+//mergesort
+
+const main = () => {
     const bar = document.createElement("span");
     bar.classList.add("bar");
 
-    const slider = document.querySelector("#slider");
-    let sliderValue = slider.value;
+    let sliderValue = SLIDER.value;
 
-    addChild(barContainer, bar, sliderValue);
+    addChild(BAR_CONTAINER, bar, sliderValue);
 
     let intArray = randomIntArray(sliderValue);
 
     updateBars(intArray);
 
-    slider.addEventListener("input", () => {
-        const sliderDifference = slider.value - sliderValue;
+    SLIDER.addEventListener("input", () => {
+        const newSliderValue = SLIDER.value;
+        const sliderDifference = newSliderValue - sliderValue;
         if (sliderDifference > 0) {
-            addChild(barContainer, bar, sliderDifference);
-            addRandomNumberTimes(intArray, sliderDifference, slider.value);
+            addChild(BAR_CONTAINER, bar, sliderDifference);
+            addRandomNumberTimes(intArray, sliderDifference, newSliderValue);
         }
         if (sliderDifference < 0) {
-            removeChild(barContainer, Math.abs(sliderDifference));
-            removeFromEnd(intArray, Math.abs(sliderDifference, slider.value));
+            removeChild(BAR_CONTAINER, Math.abs(sliderDifference));
+            removeFromEnd(intArray, Math.abs(sliderDifference, newSliderValue));
         }
-        sliderValue = slider.value;
+        sliderValue = newSliderValue;
         updateBars(intArray);
     });
+
+    QUICK_SORT_BUTTON.addEventListener("click", () => {
+        quickSort(intArray, 0, intArray.length - 1);
+    });
 };
-
-
 
 main();
