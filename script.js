ce4f19d58c0ece4a180d5c17abfb8d9267192ac5
abelHodasz <hodabi@gmail.com>
2020-03-03 10:42:05 +0100

    organize code

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const BAR_CONTAINER = document.querySelector("#bar-container");
 const SLIDER = document.querySelector("#slider");
 const QUICK_SORT_BUTTON = document.querySelector("#quick-sort");
 
-const mapRange = function(n, start1, stop1, start2, stop2) {
-    return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+const COLORS = {
+    ORANGE: "#f39c12",
+    BLUE: "#48c7eb",
+    GREEN: "#1abc9c"
 };
+//Dom manipulation
 
 function addChild(to, what, times) {
     for (let i = 0; i < times; i++) {
@@ -19,24 +22,40 @@ function removeChild(from, times) {
     }
 }
 
-function getRandomMappedNumber(min, max, minMap, maxMap) {
-    const randomNum = Math.floor(Math.random() * max) + min;
-    const randomNumMapped = mapRange(randomNum, min, max, minMap, maxMap);
-    return randomNumMapped;
-}
-
-function randomIntArray(length) {
-    return Array.from({ length }, () => {
-        return getRandomMappedNumber(0, length, MIN, HEIGHT);
-    });
-}
-
 const updateBars = intArray => {
     const bars = document.querySelectorAll(".bar");
     bars.forEach((bar, index) => {
         bar.style.height = intArray[index] + "px";
     });
 };
+
+const color = (element, color) => {
+    element.style.color = color;
+};
+
+//Utility
+
+const mapRange = function(n, start1, stop1, start2, stop2) {
+    return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+};
+
+function getRandomMappedNumber(min, max, minMap, maxMap) {
+    const randomNum = Math.floor(Math.random() * max) + min;
+    const randomNumMapped = mapRange(randomNum, min, max, minMap, maxMap);
+    return randomNumMapped;
+}
+
+const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+//Array manipulation
+
+function randomIntArray(length) {
+    return Array.from({ length }, () => {
+        return getRandomMappedNumber(0, length, MIN, HEIGHT);
+    });
+}
 
 const addRandomNumberTimes = (intArray, times, max) => {
     for (let i = 0; i < times; i++) {
@@ -49,10 +68,6 @@ const removeFromEnd = (intArray, times) => {
     for (let i = 0; i < times; i++) {
         intArray.pop();
     }
-};
-
-const sleep = ms => {
-    return new Promise(resolve => setTimeout(resolve, ms));
 };
 
 //SORTING ALGORITHMS
@@ -91,6 +106,8 @@ async function quickSort(array, low, high) {
 }
 
 //mergesort
+
+//main
 
 const main = () => {
     const bar = document.createElement("span");
